f6759ce2bf3467756ac99d70d57824d26df6ca57
nyxgeek <nyxgeek@rslabs.co>
2016-08-03 10:47:05 -0700

    added csv export

--- lyncstink.py
+++ lyncstink.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
 import urllib2
-import sys, getopt, os, ssl
+import sys, getopt, os, ssl, csv
 
 ###############################################################################
 # # # # # # # # # # # # # # # # # #    VARS    # # # # # # # # # # # # # # # #
 ###############################################################################
 
 
-subdomains = ["dialin", "meet", "lyncdiscover", "dialin", "access", "lync"]
+subdomains = ["dialin", "meet", "lyncdiscover", "dialin", "access", "lync", "lyncext", "lyncaccess01", "lyncaccess"]
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -32,7 +32,16 @@ def checkstatus(testurl):
         subdomain = subdomains[startingpoint]
         tmpdomain = subdomain + "." + testurl
         print "+ Trying %s" % tmpdomain
-
+ 
+        #first try to see if they have wildcard turned on, if so, gtfo
+        try:
+             testwildcard = urllib2.urlopen("https://a8120jksdpadsf1834jk1212319023471nasdfnv8zalsdfoqpxcv." + testurl, context=ctx, timeout=3).getcode()
+             if (testwildcard == 200):
+                print "FOUND WILDCARD DOMAIN - time to GTFO"
+                startingpoint += 1
+                break
+        except:
+              sys.stdout.write('')
 
         try: 
             responsecode = urllib2.urlopen("https://" + tmpdomain, context=ctx, timeout=3).getcode()
@@ -42,6 +51,13 @@ def checkstatus(testurl):
         startingpoint += 1
 
    return indicatorcount
+
+
+def writecsv(filename,dict):
+    with open(filename, 'wb') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in dict.items():
+           writer.writerow([key, value])
 
 ###############################################################################
 # # # # # # # # # # # # # # # # # #    MAIN    # # # # # # # # # # # # # # # #
@@ -83,3 +99,4 @@ with open(inputfilepath) as parsedfile:
 
 
     print data
+    writecsv(ofile,data)
